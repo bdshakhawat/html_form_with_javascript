b38308fc570bb3e2ed0ebf01183711dfd0bfb56f
bdshakhawat <mdshakwat.hossain@gmail.com>
2022-10-14 10:09:33 +0600

    type validation

--- helperFunc.js
+++ helperFunc.js
@@ -4,10 +4,12 @@
 // helper function for input validation.
 function typeValidation(inputType) {
     console.log("Function got input type: ",inputType)
+    // Standard list for comparison
     let allowedTypeList = ["text", "email","password"]
+    // Set default value as false
     let typeValidated = false;
 
-    // logic
+    // logic to compare with user input for validation
     if (allowedTypeList.includes(inputType.toLowerCase())) {
         typeValidated = true;
     }
@@ -28,13 +30,14 @@ function propertyToPropertyMaping(passedProperty, allowedProperty) {
 function propertyValidation(propertyObject) {
     // console.log(`Property Object ${propertyObject}`)
     const allowedPropertyList = ["type","value","placeholder"];
+    // In built function to pass the properties as parameter of propertyObject
     propertyObject.forEach(element => {
         let passedPropertyList = Object.keys(element)
         console.log(`Element: ${passedPropertyList[0]}`)
         let propertyMapingValidated = propertyToPropertyMaping(passedPropertyList, allowedPropertyList);
-        // if (!propertyMapingValidated) {
-        //     alert("Property doesn't match!")
-        //     return;
-        // }
+        if (!propertyMapingValidated) {
+            // alert("Property doesn't match!")
+            return;
+        }
     });
 };
