a74a1ac7c1b38339284d9758cdc2c71f62d3ebb8
bdshakhawat <mdshakwat.hossain@gmail.com>
2022-10-16 18:29:07 +0600

    property validation

--- helperFunc.js
+++ helperFunc.js
@@ -1,4 +1,10 @@
-// console.log("This is helper func js file!");
+
+
+console.log("hello")
+
+
+
+
 
 
 // helper function for input validation.
@@ -7,37 +13,78 @@ function typeValidation(inputType) {
     // Standard list for comparison
     let allowedTypeList = ["text", "email","password"]
     // Set default value as false
+
     let typeValidated = false;
 
     // logic to compare with user input for validation
     if (allowedTypeList.includes(inputType.toLowerCase())) {
         typeValidated = true;
+       
     }
 
-    return typeValidated            
+    return typeValidated;            
 }
+
+
 
 // property to property maping
-function propertyToPropertyMaping(passedProperty, allowedProperty) {
-    console.log("Passed property list: ", passedProperty);
-    console.log("Allowed property list: ", allowedProperty);
-    //
-    //
-    //
-}
+// function propertyToPropertyMaping(passedProperty, allowedProperty) {
+//     console.log("Passed property list: ", passedProperty);
+//     console.log("Allowed property list: ", allowedProperty);
+//     // 
+//     // 
+    
+    
+// }
+
+
+
 
 // helper function for property validation
-function propertyValidation(propertyObject) {
-    // console.log(`Property Object ${propertyObject}`)
-    const allowedPropertyList = ["type","value","placeholder"];
-    // In built function to pass the properties as parameter of propertyObject
-    propertyObject.forEach(element => {
-        let passedPropertyList = Object.keys(element)
-        console.log(`Element: ${passedPropertyList[0]}`)
-        let propertyMapingValidated = propertyToPropertyMaping(passedPropertyList, allowedPropertyList);
-        if (!propertyMapingValidated) {
-            // alert("Property doesn't match!")
-            return;
-        }
-    });
-};
+// function propertyValidation(propertyObject) {
+//     console.log(propertyObject);
+//     console.log(`Property Object ${propertyObject}`)
+//     console.log("Function got property type: ", propertyObject);
+    
+//     const allowedPropertyList = ["type","value","placeholder"];
+    
+           
+//         }
+    
+        // return propertyValidation;  
+        // let propertyMapingValidated = propertyToPropertyMaping(passedPropertyList, allowedPropertyList);
+        // if (!propertyMapingValidated) {
+        //     alert("Property doesn't match!")
+        //     return;
+        // }
+    // });
+    // if (allowedPropertyList.includes(propertyObject.toLowerCase())){
+    //     propertyValidated =true;
+        
+    // }
+    // return propertyValidated;
+    // let propertyValidated = false;
+
+    // if (allowedPropertyList.includes(propertyObject.toLowerCase())) {
+    //     propertyValidated = true;
+       
+    // }
+
+    // return propertyValidated;           
+// };
+function validationProperty(propertyObject){
+    let allowedPropertyList=["type","value","placeholder"];
+    elemObj.forEach(elements => {
+        // for (let key in elements) {
+        //         console.log(`${key}: ${elements[key]}`)
+        //         console.log(`${elements[key]}`);
+        // }
+        let passedProperty= Object.keys(elements);
+        console.log(passedProperty);
+
+    })
+}
+
+export default typeValidation
+
+    
